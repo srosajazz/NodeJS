7e631f9177dc64986f60964c1f9f4608de762a67
Sergio Rosa <scarlosjazz@gmail.com>
2019-12-27 11:03:14 -0500

    Request params

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require("express");
 
 const server = express();
 
-server.get("/test", (req, res) => {
-  //   return res.send("Hello World");
-  return res.json({ message: "Hello, Sergio" });
+// Query params = ?test=1
+// Route params = /users/1
+// Request body = { "name": "Sergio", "email": "sergio@gmail.com"}
+//localhost:3000/users/123244
+
+server.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  return res.json({ message: `Search User ${id}` });
 });
 
 server.listen(3003);
+
+/**
+ * Documents:
+ * 1.  return res.send("Hello World");
+ * return res.json({ message: "Hello, Sergio" });
+ * //   return res.json({ message: `Hello, ${name}` });
+ */
